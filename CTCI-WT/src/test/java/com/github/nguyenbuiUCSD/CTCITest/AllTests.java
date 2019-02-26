40a374b3d8ae0c5372aa241b2ebb7407794de052
will <will@will-MS>
2019-02-25 19:48:26 -0800

    Add Test Suit - All Test Cases

---
+++ CTCI-WT/src/test/java/com/github/nguyenbuiUCSD/CTCITest/AllTests.java
@@ -0,0 +1,11 @@
+package com.github.nguyenbuiUCSD.CTCITest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AnagramTest.class, SingleLinkedListTest.class, TreeTest.class })
+public class AllTests {
+
+}
