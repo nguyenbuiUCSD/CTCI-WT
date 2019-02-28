d3990053cceb3026d4174a61b366a3112acdc2fc
will <will@will-MS>
2019-02-28 00:13:53 -0800

    Add test cases

--- CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/TripleStep.java
+++ CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/TripleStep.java
@@ -10,6 +10,10 @@ public class TripleStep {
 	 * Promt: A child is running up a staircase with n steps and can hop either 1 step, 2 steps,
 	 * or 3 steps at a time. Implement a method to count how many possible ways the child can run
 	 * up the stairs.
+	 * Note: Assume findTripleStep(0) = 1. It's a lot easier if we define findTripleStep(0) = 0, this case
+	 * 		we need 2 base cases:
+	 * 				if (n < 0) return 0;
+	 * 				if (n==0) return 1;
 	 */
 	public int findTripleStep(int n) {
 		return findTripleStepRecursive(n);
@@ -24,14 +28,22 @@ public class TripleStep {
 	 * up the stairs.
 	 */
 	public int findTripleStepRecursive(int n) {
-		if (n < 0) {
+		if (n < 1) {
 			return 0;
 		}
 		
-		if (n==0) {
+		if (n==1) {
 			return 1;
 		}
+		
+		if (n==2) {
+			return 2;
+		}
 
+		if (n==3) {
+			return 4;
+		}
+		
 		return findTripleStepRecursive(n-1)+findTripleStepRecursive(n-2)+findTripleStepRecursive(n-3);
 	}
 }
