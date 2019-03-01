8a9b1d0dff30a353176166691be120edff3b2691
nguyenbuiUCSD <n4bui@ucsd.edu>
2019-03-01 01:15:14 -0800

    Add balls and bins

---
+++ CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/BinsAndBalls.java
@@ -0,0 +1,50 @@
+package com.github.nguyenbuiUCSD.CTCI;
+
+
+public class BinsAndBalls {
+	
+	/*
+	 * Function name: findWays
+	 * Param:
+	 * 		n:	number of balls
+	 * 		m:	number of bins
+	 * 		return: number of ways to put balls to bins
+	 * Promt: 	Count the number of way to put n balls to m bins, ball are the same, bins are the same
+	 * 			This can be understood as find the number of ways to find m number so that sum equal n
+	 * 			ex: n = 5, m = 3 => (0,0,5), (0,1,4),... Note that (0,0,5) is the same as (0,5,0) and (5,0,0)
+	 */
+	public int findWays(int nBalls, int mBins) {
+		return findWaysRecurSive(nBalls, mBins);
+	}
+	
+	/*
+	 * Function name: findWays
+	 * Param:
+	 * 		n:	number of balls
+	 * 		m:	number of bins
+	 * 		return: number of ways to put balls to bins
+	 * Promt: 	Count the number of way to put n balls to m bins, ball are the same, bins are the same
+	 * 			This can be understood as find the number of ways to find m number so that sum equal n
+	 * 			ex: n = 5, m = 3 => (0,0,5), (0,1,4),... Note that (0,0,5) is the same as (0,5,0) and (5,0,0)
+	 */
+	public int findWaysRecurSive(int nBalls, int mBins) {
+		//Base cases
+		if (nBalls <= 1) {
+			return 1;
+		}
+		if (mBins < 1) {
+			return 0;
+		}
+		if (mBins == 1) {
+			return 1;
+		}
+		//If bins are more than balls, we can ignore  the empty bins
+		if (nBalls<mBins) {
+			return findWays(nBalls, nBalls);
+		}
+		//The number of ways is the total of number of ways when one bin is empty + the number
+		//of ways when there is no empty bin (But one ball to each bin, (nBalls - mBins), then
+		//randomly distribute the rest) 
+		return findWays(nBalls, mBins-1) + findWays(nBalls-mBins, mBins);
+	}
+}
---
+++ CTCI-WT/src/test/java/com/github/nguyenbuiUCSD/CTCITest/BinsAndBallsTest.java
@@ -0,0 +1,83 @@
+package com.github.nguyenbuiUCSD.CTCITest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+import com.github.nguyenbuiUCSD.CTCI.BinsAndBalls;
+
+public class BinsAndBallsTest {
+
+	BinsAndBalls bnb = new BinsAndBalls();
+	
+	@Test
+	public void findWaysTest1() {
+		assertEquals(1, bnb.findWays(0, 0));
+	}
+	
+	@Test
+	public void findWaysTest2() {
+		assertEquals(1, bnb.findWays(0, 10));
+	}
+	
+	@Test
+	public void findWaysTest3() {
+		assertEquals(1, bnb.findWays(1, 10));
+	}
+	
+	@Test
+	public void findWaysTest4() {
+		assertEquals(2, bnb.findWays(2, 2));
+	}
+	
+	@Test
+	public void findWaysTest5() {
+		// Run 100 times
+		for (int times = 0;times<10;times++) {
+			Random random = new Random();
+			List<int[]> list = new ArrayList<int[]>();
+	        int balls = random.nextInt(100);
+			int arr[]= {0,0,0,0};
+			int numOfWays=1;
+	        
+	        for (int i = 0; i<balls;i++)
+	        	for (int j=0;j<balls;j++)
+	        		for (int k=0;k<balls;k++)
+	        			for (int l=0;l<balls;l++){
+	        				if (i+j+k+l==balls) {
+	        					arr[0] = i;
+	        					arr[1] = j;
+	        					arr[2] = k;
+	        					arr[3] = l;
+	        				
+	        					Arrays.sort(arr);
+	        					
+	        					boolean duplicate = false;
+	        					for (int[] it: list) {
+	        						if (Arrays.equals(it,arr)) {
+	        							duplicate = true;
+	        							break;
+	        						}
+	        					}
+	        				
+	        					if (duplicate==false) {
+	        						numOfWays++;
+	        						list.add(new int[]{arr[0],arr[1],arr[2],arr[3]}); 
+	        					}
+	        				}
+	        }
+			
+			assertEquals(numOfWays, bnb.findWays(balls, 4));
+			System.out.println(numOfWays);
+		}
+
+	}
+	
+	
+
+}
