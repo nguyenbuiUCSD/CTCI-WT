2c2dc9c4836facf07effd9fad1d64a176702a696
nguyenbuiUCSD <n4bui@ucsd.edu>
2019-03-01 01:36:23 -0800

    Add balls and bins Dynamic Programing

--- CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/BinsAndBalls.java
+++ CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/BinsAndBalls.java
@@ -1,5 +1,6 @@
 package com.github.nguyenbuiUCSD.CTCI;
 
+import java.util.Arrays;
 
 public class BinsAndBalls {
 	
@@ -14,7 +15,7 @@ public class BinsAndBalls {
 	 * 			ex: n = 5, m = 3 => (0,0,5), (0,1,4),... Note that (0,0,5) is the same as (0,5,0) and (5,0,0)
 	 */
 	public int findWays(int nBalls, int mBins) {
-		return findWaysRecurSive(nBalls, mBins);
+		return findWaysDP(nBalls, mBins);
 	}
 	
 	/*
@@ -46,5 +47,77 @@ public class BinsAndBalls {
 		//of ways when there is no empty bin (But one ball to each bin, (nBalls - mBins), then
 		//randomly distribute the rest) 
 		return findWays(nBalls, mBins-1) + findWays(nBalls-mBins, mBins);
+	}
+	
+	/*
+	 * Function name: findWays
+	 * Param:
+	 * 		n:	number of balls
+	 * 		m:	number of bins
+	 * 		return: number of ways to put balls to bins
+	 * Promt: 	Count the number of way to put n balls to m bins, ball are the same, bins are the same
+	 * 			This can be understood as find the number of ways to find m number so that sum equal n
+	 * 			ex: n = 5, m = 3 => (0,0,5), (0,1,4),... Note that (0,0,5) is the same as (0,5,0) and (5,0,0)
+	 */
+	public int findWaysDP(int nBalls, int mBins) {
+		//Base cases
+		if (nBalls <= 1) {
+			return 1;
+		}
+		if (mBins < 1) {
+			return 0;
+		}
+		if (mBins == 1) {
+			return 1;
+		}
+		//If bins are more than balls, we can ignore  the empty bins
+		if (nBalls<mBins) {
+			return findWays(nBalls, nBalls);
+		}
+		//The number of ways is the total of number of ways when one bin is empty + the number
+		//of ways when there is no empty bin (But one ball to each bin, (nBalls - mBins), then
+		//randomly distribute the rest)
+		int memo[][] = new int[nBalls][nBalls];
+		for (int[] row:memo) {
+		 Arrays.fill(row, -1);		
+		}
+
+		return findWaysDP( nBalls,  mBins,  memo);
+	}
+	
+	/*
+	 * Function name: findWays
+	 * Param:
+	 * 		n:	number of balls
+	 * 		m:	number of bins
+	 * 		return: number of ways to put balls to bins
+	 * Promt: 	Count the number of way to put n balls to m bins, ball are the same, bins are the same
+	 * 			This can be understood as find the number of ways to find m number so that sum equal n
+	 * 			ex: n = 5, m = 3 => (0,0,5), (0,1,4),... Note that (0,0,5) is the same as (0,5,0) and (5,0,0)
+	 */
+	public int findWaysDP(int nBalls, int mBins, int memo[][]) {
+		//Base cases
+		if (nBalls <= 1) {
+			return 1;
+		}
+		if (mBins < 1) {
+			return 0;
+		}
+		if (mBins == 1) {
+			return 1;
+		}
+		
+		//If bins are more than balls, we can ignore  the empty bins
+		if (nBalls<mBins) {
+			return findWays(nBalls, nBalls);
+		}
+		
+		if (memo[nBalls-1][mBins-1]==-1) {
+			memo[nBalls-1][mBins-1] = findWaysDP(nBalls, mBins-1,memo) + findWaysDP(nBalls-mBins, mBins,memo);
+		}
+		//The number of ways is the total of number of ways when one bin is empty + the number
+		//of ways when there is no empty bin (But one ball to each bin, (nBalls - mBins), then
+		//randomly distribute the rest) 
+		return memo[nBalls-1][mBins-1];
 	}
 }
