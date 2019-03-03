b080637432159b01129ab2076461a8bd7da27dc9
nguyenbuiUCSD <n4bui@ucsd.edu>
2019-03-02 23:48:20 -0800

    Add more test cases mergeTwoList

--- CTCI-WT/src/test/java/com/github/nguyenbuiUCSD/CTCITest/SingleLinkedListTestMergeList.java
+++ CTCI-WT/src/test/java/com/github/nguyenbuiUCSD/CTCITest/SingleLinkedListTestMergeList.java
@@ -20,7 +20,7 @@ public class SingleLinkedListTestMergeList {
 		SingleLinkedList singleLinkedList2 = new SingleLinkedList() {{
 			insertNode('B');
 			insertNode('D');
-			insertNode('F');;
+			insertNode('F');
 			insertNode('H');
 		}};
 		
@@ -28,6 +28,44 @@ public class SingleLinkedListTestMergeList {
 		SingleLinkedList singleLinkedList3 = new SingleLinkedList(mergeList.mergeTwoList(
 						singleLinkedList1.gethead(), singleLinkedList2.gethead()));
 		assertEquals("ABCDEFGH", singleLinkedList3.toString());
+	}
+	
+	@Test
+	public void test2() {
+		SingleLinkedList singleLinkedList1 = new SingleLinkedList() {{
+			insertNode('A');
+		}};
+		SingleLinkedList singleLinkedList2 = new SingleLinkedList();
+		
+		MergeList mergeList = new MergeList();
+		SingleLinkedList singleLinkedList3 = new SingleLinkedList(mergeList.mergeTwoList(
+						singleLinkedList1.gethead(), singleLinkedList2.gethead()));
+		assertEquals("A", singleLinkedList3.toString());
+	}
+	
+	@Test
+	public void test3() {
+		SingleLinkedList singleLinkedList1 = new SingleLinkedList();
+		SingleLinkedList singleLinkedList2 = new SingleLinkedList();
+		
+		MergeList mergeList = new MergeList();
+		SingleLinkedList singleLinkedList3 = new SingleLinkedList(mergeList.mergeTwoList(
+						singleLinkedList1.gethead(), singleLinkedList2.gethead()));
+		assertEquals("", singleLinkedList3.toString());
+	}
+	
+	@Test
+	public void test4() {
+		SingleLinkedList singleLinkedList1 = new SingleLinkedList();
+		SingleLinkedList singleLinkedList2 = new SingleLinkedList() {{
+			insertNode('A');
+		}};
+
+		
+		MergeList mergeList = new MergeList();
+		SingleLinkedList singleLinkedList3 = new SingleLinkedList(mergeList.mergeTwoList(
+						singleLinkedList1.gethead(), singleLinkedList2.gethead()));
+		assertEquals("A", singleLinkedList3.toString());
 	}
 
 }
