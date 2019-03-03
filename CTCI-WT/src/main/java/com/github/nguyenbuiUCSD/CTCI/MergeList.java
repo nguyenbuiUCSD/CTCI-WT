b7b452415c62a446034bfa1a5de9adf0bd519d38
nguyenbuiUCSD <n4bui@ucsd.edu>
2019-03-02 23:58:47 -0800

    Add merge sorted list non recursive

--- CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/MergeList.java
+++ CTCI-WT/src/main/java/com/github/nguyenbuiUCSD/CTCI/MergeList.java
@@ -11,7 +11,7 @@ public class MergeList {
 	 * Note: O(n+m) space and time if using recursion. O(1) space if use non recursive solution
 	 */
 	public SLLNode mergeTwoList(SLLNode l1, SLLNode l2) {
-		return mergeTwoListRecursive(l1,l2);
+		return mergeTwoListNonRecursive(l1,l2);
 	}
 	
 	/*
@@ -38,6 +38,38 @@ public class MergeList {
             l2.next=mergeTwoListRecursive(l1, l2.next);
             return l2;
         }
+	}
         
+    	/*
+    	 * function name: mergeTwoList
+    	 * param:
+    	 * 		list1: first list
+    	 * 		list2: second list
+    	 * 		return:		new merged List
+    	 * Usage: Merge two sorted list
+    	 * Note: O(n+m) space and time if using recursion
+    	 */
+	private SLLNode mergeTwoListNonRecursive(SLLNode l1, SLLNode l2) {
+        SLLNode current = new SLLNode('Z');
+        SLLNode ret = current;
+        
+        while(l1!=null&&l2!=null) {
+        	if (l1.data<l2.data) {
+        		current.next = l1;
+        		l1=l1.next;
+        	} else {
+        		current.next=l2;
+        		l2=l2.next;
+        	}
+        	current=current.next;
+        }
+        
+        if (l1==null) {
+        	current.next=l2;
+        } else {
+        	current.next=l1;
+        }
+        
+        return ret.next;
 	}
 }
